01ba7014d34c3d97939d58d06983fab49350a743
shehanah <shehanah60@gmail.com>
2024-11-25 17:04:06 +0300

    added

---
+++ Admin.py
@@ -0,0 +1,4 @@
+class Admin:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
---
+++ Workshop.py
@@ -0,0 +1,13 @@
+from random import random
+
+
+class Worksshop:
+    def __init__(self,name,location,capacity,date,time,number):
+        self.name = name
+        self.location = location
+        self.capacity = capacity
+        self.date = date
+        self.time = time
+        self.number = number
+
+
